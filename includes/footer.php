3e7391e095e75957fff656dc6bc2da2edaf8888f
Fayzullo <92411568+fayzullo1501@users.noreply.github.com>
2022-10-07 13:08:12 +0300

    intent data base
    data base (php & mysql)

---
+++ includes/footer.php
@@ -0,0 +1,3 @@
+<footer class="footer text-center text-muted">
+&copy; <?php echo date("Y"); ?> - INTENT Management System - Developed by <a href="https://fayxanur.netlify.app"><b>FAYXANUR</b></a>
+</footer>
